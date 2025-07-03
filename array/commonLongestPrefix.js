3eae08aee1d1c3f90ada9f925e21d310dbee14da
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-03 23:41:43 +0500

    Optimize the code with array empty use cases

--- array/commonLongestPrefix.js
+++ array/commonLongestPrefix.js
@@ -1,4 +1,6 @@
 function commonLongestPrefix(arr){ 
+    if(arr.length === 0) return "the array is empty";
+
      for(let i=0; i<arr[0].length; i++){ 
         let char = arr[0][i]; 
         for(let j = 0; j < arr.length; j++){ 
@@ -7,6 +9,8 @@ function commonLongestPrefix(arr){
             }
         }
     }
+
+    return arr[0];
 }
 
 let array = ["flower", "flow","flutter"];
