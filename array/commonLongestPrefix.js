2156bf7b4279cf6b60cc4f803a972a6796752b7d
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-03 23:40:21 +0500

    remove the console logs for debugging

--- array/commonLongestPrefix.js
+++ array/commonLongestPrefix.js
@@ -1,18 +1,8 @@
-function commonLongestPrefix(arr){
-    console.log(arr[0].length);
-    console.log(arr[1].slice(0,2));
-    console.log(arr.length);
-    for(let i=0; i<arr[0].length; i++){
-        console.log(i);
-        console.log(`arr[0][${i}]`);
-        let char = arr[0][i];
-        console.log(char);
-        for(let j = 0; j < arr.length; j++){
-            console.log(j);
-            console.log(char);
-            if(arr[j][i] != char){
-                console.log("if block");
-                console.log(arr[0].slice(0,i));
+function commonLongestPrefix(arr){ 
+     for(let i=0; i<arr[0].length; i++){ 
+        let char = arr[0][i]; 
+        for(let j = 0; j < arr.length; j++){ 
+            if(arr[j][i] != char){ 
                 return arr[0].slice(0,i);
             }
         }
