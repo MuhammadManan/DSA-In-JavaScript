4c28f943ec069d8d67ef6648046ee70e8414f6a4
Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-07 09:24:13 +0500

    Union of two sorted array using two pointer approach

--- array/unionOfTwoSortedArray.js
+++ array/unionOfTwoSortedArray.js
@@ -1,17 +1,60 @@
+
+// using the Brute force approach .... 
+
+// function unionOfTwoSortedArrays(arr1, arr2){
+//     const union = new Set();
+//     for(let value of arr1){
+//         union.add(value);
+//     }
+//     for(let value of arr2){
+//         union.add(value);
+//     }
+//     // console.log(union);
+//     return union;
+
+// }
+
+// Using the two pointer approach for optimal approach.....
+
 function unionOfTwoSortedArrays(arr1, arr2){
-    const union = new Set();
-    for(let value of arr1){
-        union.add(value);
-    }
-    for(let value of arr2){
-        union.add(value);
-    }
-    // console.log(union);
-    return union;
+    let i = 0; 
+    let j = 0; 
+    const unionArray = new Array();
 
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]<= arr2[j]){
+            if(arr1.length === 0 || unionArray.at(-1) != arr1[i]){
+                unionArray.push(arr1[i]);
+            }
+            i++;
+        }
+        else{
+            if(arr2[j]<=arr1[i]){
+                if(arr2.length === 0 || unionArray.at(-1) != arr2[j]){
+                    unionArray.push(arr2[j]);
+                }
+                j++;
+            }
+        }
+    }
 
+    while(j< arr2.length){
+        if(arr2.length === 0 || unionArray.at(-1) != arr2[j] ){
+            unionArray.push(arr2[j])
+        }
+        j++;
+    }
+
+    while(i < arr1.length){
+        if(arr1.length === 0 || unionArray.at(-1) != arr1[i]){
+            unionArray.push(arr1[i]);
+        }
+        i++;
+    }
+
+    return unionArray;
 }
 
-const array1 = [1, 2, 3, 2, 1, "apple", "ban"];
-const array2 = [2, 3, 4, "cap", "apple", 5, 6];
+const array1 = [1, 1, 2, 3, ];
+const array2 = [2, 3, 4, 5, 6];
 console.log(unionOfTwoSortedArrays(array1, array2));
