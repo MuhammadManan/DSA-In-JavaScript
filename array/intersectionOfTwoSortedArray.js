79edcd3d89519edd09e466e578049dee6d697c03
Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-08 02:44:10 +0500

    using the brute force with the visited tracking array...

--- array/intersectionOfTwoSortedArray.js
+++ array/intersectionOfTwoSortedArray.js
@@ -1,13 +1,36 @@
+// using the brute force with the tracking index
+
+// function intersectionOfTwoSortedArray(arr1, arr2){
+//     const intersectArray = new Array();
+//     let tempIndex = 0; 
+//     for(let i=0; i< arr1.length; i++){
+//         for(let j=tempIndex; j < arr2.length; j++){
+//             if(arr1[i]==arr2[j]){
+//                 intersectArray.push(arr1[i]); 
+//                 tempIndex = j+1;
+//                 break;
+//             }else if(arr2[j]>arr1[i]){
+//                 break;
+//             }
+//         }
+//     }
+//     return intersectArray;
+// }
+
+
+// using the brute force with the visited tracking array...
+
 function intersectionOfTwoSortedArray(arr1, arr2){
     const intersectArray = new Array();
-    let tempIndex = 0; 
-    for(let i=0; i< arr1.length; i++){
-        for(let j=tempIndex; j < arr2.length; j++){
-            if(arr1[i]==arr2[j]){
-                intersectArray.push(arr1[i]); 
-                tempIndex = j+1;
+    const visitedArray = new Array(arr2.length).fill(0);
+    for(let i=0; i<arr1.length; i++){
+        for(let j=0; j<arr2.length; j++){
+            if(arr1[i]==arr2[j] && visitedArray[j] == 0 ){
+                intersectArray.push(arr1[i]);
+                visitedArray[j] = 1;
                 break;
-            }else if(arr2[j]>arr1[i]){
+            }
+            if(arr2[j]>arr1[i]){
                 break;
             }
         }
