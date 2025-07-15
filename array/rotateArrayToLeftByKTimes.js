3e48258398243397bca6145d6d6c302c01bb02f0
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-15 14:12:21 +0500

    Reverse the array of start to k-1, k to end...

--- array/rotateArrayToLeftByKTimes.js
+++ array/rotateArrayToLeftByKTimes.js
@@ -10,8 +10,30 @@ function rotateLeftByKTimes(arr, k){
     return mergeArray;
 }
 
-const array = [1,2,3,4,5];
-const k = 7;
+function leftRotateArrayByKTimesUsingTwoPointer(arr, k){
+    let n = arr.length;
+    k = k % n; 
+    let sk = k-1;
+    let ek = k;
+    let s = 0 , e =n-1;
+    while(s<sk || ek < e ){
+        if(s !== sk){
+            [arr[s], arr[sk]] = [arr[sk], arr[s]];
+            s++;
+            sk--;
+        }
+        if(ek !== e){
+            [arr[ek], arr[e]] = [arr[e], arr[ek]];
+            ek++;
+            e--;
+        }
+    }
+    console.log(arr);
+}
+
+const array = [1,2,3,4,5,6,7,8,9,10];
+const k = 3;
+console.log(leftRotateArrayByKTimesUsingTwoPointer(array, k));
 console.log(rotateLeftByKTimes(array,k ));
 // console.log(newArr);
 // console.log(mergeArray);
