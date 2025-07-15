eb552103e337180d19a17c0effb99be8dafd5a08
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-15 14:59:58 +0500

    Optimize the code using the one reverse function for all them..

--- array/rotateArrayToLeftByKTimes.js
+++ array/rotateArrayToLeftByKTimes.js
@@ -12,39 +12,53 @@ function rotateLeftByKTimes(arr, k){
     return mergeArray;
 }
 
+function reverseArray(arr, start, end){
+    while(start < end){
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
+
 function leftRotateArrayByKTimesUsingTwoPointer(arr, k){
     let n = arr.length;
     k = k % n; 
-    let sk = k-1;
-    let ek = k;
-    let s = 0 , e =n-1;
-    while(s<sk || ek < e ){
-        if(s !== sk){
-            [arr[s], arr[sk]] = [arr[sk], arr[s]];
-            s++;
-            sk--;
-        }
-        if(ek !== e){
-            [arr[ek], arr[e]] = [arr[e], arr[ek]];
-            ek++;
-            e--;
-        }
-    }
-    console.log(arr);
+    // let sk = k-1;
+    // let ek = k;
+    // let s = 0 , e =n-1;
+    // while(s<sk || ek < e ){
+    //     if(s !== sk){
+    //         [arr[s], arr[sk]] = [arr[sk], arr[s]];
+    //         s++;
+    //         sk--;
+    //     }
+    //     if(ek !== e){
+    //         [arr[ek], arr[e]] = [arr[e], arr[ek]];
+    //         ek++;
+    //         e--;
+    //     }
+    // }
+
+    reverseArray(arr, 0, k-1);
+    reverseArray(arr, k, n-1);
+    reverseArray(arr, 0, n-1);
+    // console.log(arr);
     
-    let left = 0, right = n -1;
-    while(left<right){
-        [arr[left], arr[right]] = [arr[right], arr[left]];
-        left++;
-        right--;
-    }
-    console.log(arr);
+    // let left = 0, right = n -1;
+    // while(left<right){
+    //     [arr[left], arr[right]] = [arr[right], arr[left]];
+    //     left++;
+    //     right--;
+    // }
+    // console.log(arr);
+    return arr;
 }
 
 const array1 = [1,2,3,4,5,6,7,8,9,10];
 const array2 = [1,2,3,4,5,6,7,8,9,10];
 const k = 3;
 console.log(leftRotateArrayByKTimesUsingTwoPointer(array1, k));
-console.log(rotateLeftByKTimes(array2,k ));
+// console.log(rotateLeftByKTimes(array2,k ));
+// console.log("reverse array 1 : ", array1);
 // console.log(newArr);
 // console.log(mergeArray);
