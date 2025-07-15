131ce0dc2aef01814d2c2e5bcbdcb431fe87dfcc
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-15 14:21:13 +0500

    Rotate the array to left by k times using the two pointer appraoch...

--- array/rotateArrayToLeftByKTimes.js
+++ array/rotateArrayToLeftByKTimes.js
@@ -2,11 +2,13 @@
 function rotateLeftByKTimes(arr, k){
     k = k % arr.length;
     const newArr1 = arr.slice(0,k);
+    console.log(newArr1);
     const newArr2 = arr.slice(k, arr.length); 
+    console.log(newArr2);
     // const mergeArray = [...newArr2, ...newArr1];
     // we can also use the concat function here
     const mergeArray = newArr2.concat(newArr1);
-    // console.log(mergeArray);
+    console.log(mergeArray);
     return mergeArray;
 }
 
@@ -29,11 +31,20 @@ function leftRotateArrayByKTimesUsingTwoPointer(arr, k){
         }
     }
     console.log(arr);
+    
+    let left = 0, right = n -1;
+    while(left<right){
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+    console.log(arr);
 }
 
-const array = [1,2,3,4,5,6,7,8,9,10];
+const array1 = [1,2,3,4,5,6,7,8,9,10];
+const array2 = [1,2,3,4,5,6,7,8,9,10];
 const k = 3;
-console.log(leftRotateArrayByKTimesUsingTwoPointer(array, k));
-console.log(rotateLeftByKTimes(array,k ));
+console.log(leftRotateArrayByKTimesUsingTwoPointer(array1, k));
+console.log(rotateLeftByKTimes(array2,k ));
 // console.log(newArr);
 // console.log(mergeArray);
