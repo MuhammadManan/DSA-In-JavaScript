44576fef5235732c2989210f1fde4aadc7774618
Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-07-16 23:27:02 +0500

    insert element in an array...

--- array/unionOfTwoSortedArray.js
+++ array/unionOfTwoSortedArray.js
@@ -57,5 +57,5 @@ function unionOfTwoSortedArrays(arr1, arr2){
 
 const array1 = [1, 1, 2, 3, ];
 const array2 = [2, 3, 4, 5, 6];
-const array3 = [1, 2, 3, 4, 5, 6]; 
+const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 console.log(unionOfTwoSortedArrays(array1, array2));
