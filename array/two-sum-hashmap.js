9cd389712b48787f2f3f46107547c6701c2c2766
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-02 22:56:56 +0500

    Set Up file with pre define template to solve the problem

--- array/two-sum-hashmap.js
+++ array/two-sum-hashmap.js
@@ -6,5 +6,20 @@ Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
 */
 
+/**
+ * Two Sum - HashMap Solution
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+function twoSum(nums, target) {
+  // Write your code 
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(nums, target)); // Expected output: [0, 1]
 
 
