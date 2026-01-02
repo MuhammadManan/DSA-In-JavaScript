52adea1ffe859650bdf019501418ad6df0ae8bea
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-02 22:54:43 +0500

    Set up file with problem statement

---
+++ array/two-sum-hashmap.js
@@ -0,0 +1,10 @@
+/*
+Problem Name: Two Sum (Unsorted Array)
+Problem Statement
+
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+*/
+
+
+
