7236b4e275547f66f11b44c11c90cd9b92baba99
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-02 23:12:14 +0500

    Solve Two Sum using HashMap in JavaScript

--- array/two-sum-hashmap.js
+++ array/two-sum-hashmap.js
@@ -15,6 +15,23 @@ return indices of the two numbers such that they add up to target.
 
 function twoSum(nums, target) {
   // Write your code 
+  let map={};
+  console.log(map);
+
+  for(let i=0; i<nums.length; i++){
+    console.log("i : ", i);
+    let complement = target - nums[i];
+    console.log("complement : ", complement);
+    
+    if(map[complement] !== undefined){
+        console.log('map inside if : ', map);
+        return [map[complement],i];
+    }
+
+    map[nums[i]] = i;
+    console.log("map : ", map);
+  }
+
 }
 
 const nums = [2, 7, 11, 15];
