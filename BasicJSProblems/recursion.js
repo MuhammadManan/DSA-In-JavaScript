c249412491a87380454aa138432d47025f4cc390
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-04 23:49:23 +0500

    Solved the Problem to Print 1 to N through Recursion

--- BasicJSProblems/recursion.js
+++ BasicJSProblems/recursion.js
@@ -1,6 +1,14 @@
 function print1ToN(n) {
   // use recursion
-   
+  // base case
+  if(n===1){
+    console.log(1);
+    return;
+  }
+
+  // recursive case
+  print1ToN(n-1);
+  console.log(n);
 }
 
 print1ToN(5);
