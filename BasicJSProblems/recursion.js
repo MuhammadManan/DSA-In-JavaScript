4667088fe2b917b85d2adfd0ebf97e436cb86574
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-04 23:50:27 +0500

    find the factorial through Recursion

--- BasicJSProblems/recursion.js
+++ BasicJSProblems/recursion.js
@@ -1,14 +1,22 @@
-function print1ToN(n) {
-  // use recursion
-  // base case
-  if(n===1){
-    console.log(1);
-    return;
-  }
+// function print1ToN(n) {
+//   // use recursion
+//   // base case
+//   if(n===1){
+//     console.log(1);
+//     return;
+//   }
 
-  // recursive case
-  print1ToN(n-1);
-  console.log(n);
+//   // recursive case
+//   print1ToN(n-1);
+//   console.log(n);
+// }
+
+// print1ToN(5);
+
+//**************************** */
+
+function factorial(n) {
+  // return factorial of n
 }
 
-print1ToN(5);
+factorial(5);
