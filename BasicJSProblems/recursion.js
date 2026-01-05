c92d439edc2300d2711acf50fa8cc5837cac8434
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-05 12:24:46 +0500

    Solved Max of Array using Recursion

--- BasicJSProblems/recursion.js
+++ BasicJSProblems/recursion.js
@@ -30,14 +30,34 @@
 
 // **************************************
 
-function power(x, n) { 
-  // Base case
-  if(n===0){
-    return 1;
-  }
+// function power(x, n) { 
+//   // Base case
+//   if(n===0){
+//     return 1;
+//   }
 
-  // Recursive case
-  return x * power(x, n-1);
+//   // Recursive case
+//   return x * power(x, n-1);
+// }
+
+// console.log(power(2, 3));
+
+/*********************************** */
+
+function findMax(arr, i = 0,) {
+  // return max value
+  if(!arr || arr.length === 0 ) return null;
+  // Base Case
+  if(i === arr.length-1) return arr[i];
+
+  // Recursive Call
+  let max = findMax(arr, i+1);
+  if(arr[i] > max){
+    max = arr[i];
+    return max;
+  }
+  return max;
 }
 
-console.log(power(2, 3));
+// console.log(findMax([]));
+console.log(findMax([3, 7, 10, 2, 9]));
