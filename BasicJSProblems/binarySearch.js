829bfc2dfcb75d4e6aaa0cc8394efa439fb8b317
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-06 13:55:42 +0500

    Solved FirstOccurence through Binary search using iterative

--- BasicJSProblems/binarySearch.js
+++ BasicJSProblems/binarySearch.js
@@ -25,28 +25,95 @@
 
 // **************************************************
 
-function binarySearchRecursive(arr, target, low = 0, high = arr.length - 1) {
-  // return index or -1
+// function binarySearchRecursive(arr, target, low = 0, high = arr.length - 1) {
+//   // return index or -1
+
+//   if(!arr || arr.length === 0 ) return -1;
+//   // Base case
+//   if(low > high) return -1;
+
+//   let mid = Math.floor((low+high)/2);
+
+//   if(arr[mid] === target) return mid;
+
+//   // Recursive case
+//   if(arr[mid] < target){
+//     return binarySearchRecursive(arr, target, mid + 1, high);
+//   }else{
+//     return binarySearchRecursive(arr, target, low, mid - 1);
+//   }
+
+// }
+
+
+// let arr = [1, 3, 5, 7, 9, 11];
+// let target = 7;
+
+// console.log(binarySearchRecursive(arr, target));
+
+
+/************************************** */
+
+// first Occurence through binary search using iterative approach
+
+function firstOccurence(arr, target){
 
   if(!arr || arr.length === 0 ) return -1;
-  // Base case
-  if(low > high) return -1;
 
-  let mid = Math.floor((low+high)/2);
+  let low = 0; 
+  let high = arr.length - 1; 
+  let result = -1; 
 
-  if(arr[mid] === target) return mid;
-
-  // Recursive case
-  if(arr[mid] < target){
-    return binarySearchRecursive(arr, target, mid + 1, high);
-  }else{
-    return binarySearchRecursive(arr, target, low, mid - 1);
+  while(low<=high){
+    let mid = Math.floor((low+high)/2);
+    if(arr[mid]===target){
+      result = mid;
+      high = mid - 1; 
+    }else if(arr[mid]< target){
+      low = mid + 1; 
+    }else{
+      high = mid - 1; 
+    }
   }
 
+  return result;
 }
 
+let arr = [1, 2, 2, 2, 2, 3, 4];
+let target = 2;
 
-let arr = [1, 3, 5, 7, 9, 11];
-let target = 7;
+console.log(firstOccurence(arr,target));
 
-console.log(binarySearchRecursive(arr, target));
+/****************************************** */
+
+// function firstOccurenceRec(arr, target, low = 0, high = arr.length - 1){
+  
+//   if(!arr) return -1;
+
+//   if(low > high) return -1;
+
+//   let mid = Math.floor((low+high)/2);
+//   console.log("mid :", mid);
+
+//   if(arr[mid] === target){
+//      let result = firstOccurenceRec(arr, target, low, mid -1);
+//      if(result === -1){
+//         return mid;
+//      }else if( result < mid){
+//         return result;
+//      }
+//   }
+
+//   if(arr[mid] < target){
+//     return firstOccurenceRec(arr, target, mid + 1, high);
+//   }else{
+//     return firstOccurenceRec(arr, target, low, mid - 1);
+//   }
+
+// }
+
+
+// let arr = [1, 2, 2, 2, 2, 3, 4];
+// let target = 2;
+
+// console.log(firstOccurenceRec(arr,target));
