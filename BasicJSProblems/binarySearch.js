c07ca9ecc6e01eadbfb407ed40ad816c43b302d3
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-06 14:06:47 +0500

    Solved lastOccurence through Binary search using iterative

--- BasicJSProblems/binarySearch.js
+++ BasicJSProblems/binarySearch.js
@@ -86,35 +86,103 @@
 
 /****************************************** */
 
-function firstOccurenceRec(arr, target, low = 0, high = arr.length - 1){
+// function firstOccurenceRec(arr, target, low = 0, high = arr.length - 1){
   
-  if(!arr) return -1;
+//   if(!arr) return -1;
 
-  if(low > high) return -1;
+//   if(low > high) return -1;
 
-  let mid = Math.floor((low+high)/2);
-  console.log("mid :", mid);
+//   let mid = Math.floor((low+high)/2);
+//   console.log("mid :", mid);
 
-  if(arr[mid] === target){
-     let result = firstOccurenceRec(arr, target, low, mid -1);
-    //  if(result === -1){
-    //     return mid;
-    //  }else if( result < mid){
-    //     return result;
-    //  }
-      return result !== -1 ? result : mid ; 
+//   if(arr[mid] === target){
+//      let result = firstOccurenceRec(arr, target, low, mid -1);
+//     //  if(result === -1){
+//     //     return mid;
+//     //  }else if( result < mid){
+//     //     return result;
+//     //  }
+//       return result !== -1 ? result : mid ; 
+//   }
+
+//   if(arr[mid] < target){
+//     return firstOccurenceRec(arr, target, mid + 1, high);
+//   }else{
+//     return firstOccurenceRec(arr, target, low, mid - 1);
+//   }
+
+// }
+
+
+// let arr = [1, 2, 2, 2, 2, 3, 4];
+// let target = 2;
+
+// console.log(firstOccurenceRec(arr,target));
+
+// *****************************************
+
+
+// last Occurence through binary search using iterative approach
+
+function lastOccurence(arr, target){
+
+  if(!arr || arr.length === 0 ) return -1;
+
+  let low = 0; 
+  let high = arr.length - 1; 
+  let result = -1; 
+
+  while(low<=high){
+    let mid = Math.floor((low+high)/2);
+    if(arr[mid]===target){
+      result = mid;
+      low = mid + 1; 
+    }else if(arr[mid]< target){
+      low = mid + 1; 
+    }else{
+      high = mid - 1; 
+    }
   }
 
-  if(arr[mid] < target){
-    return firstOccurenceRec(arr, target, mid + 1, high);
-  }else{
-    return firstOccurenceRec(arr, target, low, mid - 1);
-  }
-
+  return result;
 }
-
 
 let arr = [1, 2, 2, 2, 2, 3, 4];
 let target = 2;
 
-console.log(firstOccurenceRec(arr,target));
+console.log(lastOccurence(arr,target));
+
+/****************************************** */
+
+// function lastOccurenceRec(arr, target, low = 0, high = arr.length - 1){
+  
+//   if(!arr) return -1;
+
+//   if(low > high) return -1;
+
+//   let mid = Math.floor((low+high)/2);
+//   console.log("mid :", mid);
+
+//   if(arr[mid] === target){
+//      let result = lastOccurenceRec(arr, target, mid+1, high);
+//     //  if(result === -1){
+//     //     return mid;
+//     //  }else if( result < mid){
+//     //     return result;
+//     //  }
+//       return result !== -1 ? result : mid ; 
+//   }
+
+//   if(arr[mid] < target){
+//     return lastOccurenceRec(arr, target, mid + 1, high);
+//   }else{
+//     return lastOccurenceRec(arr, target, low, mid - 1);
+//   }
+
+// }
+
+
+// let arr = [1, 2, 2, 2, 2, 3, 4];
+// let target = 2;
+
+// console.log(lastOccurenceRec(arr,target));
