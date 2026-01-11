d4cae54ac988de1601f6a7585dec098c1552dafc
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-11 21:14:51 +0500

    working on the next greater element using stack

--- BasicJSProblems/stack.js
+++ BasicJSProblems/stack.js
@@ -1,25 +1,59 @@
-function isValidParentheses(s){
-    let stack = [];
+// function isValidParentheses(s){
+//     let stack = [];
 
-  const map = {
-    ')': '(',
-    '}': '{',
-    ']': '['
-  };
+//   const map = {
+//     ')': '(',
+//     '}': '{',
+//     ']': '['
+//   };
 
-  for(let ch of s){
-    if(ch === "(" || ch === "{" || ch === "["){
-        stack.push(ch);
+//   for(let ch of s){
+//     if(ch === "(" || ch === "{" || ch === "["){
+//         stack.push(ch);
+//     }
+//     else{
+//         if(stack.length === 0) return false;
+//         let top = stack.pop();
+//         if(top !== map[ch]) return false; 
+//     }
+//   }
+
+//   return stack.length === 0; 
+// }
+
+// console.log(isValidParentheses("([{}])"));
+// // console.log(isValidParentheses("([)]"));
+
+/********************************************************* */
+
+function nextGreaterElement(arr) {
+  // your code here
+  let outputArray = [];
+  let stack = [];
+  for(let i=0; i<arr.length; i++){
+    if(stack.length === 0){
+        stack.push(arr[i]);
     }
-    else{
-        if(stack.length === 0) return false;
-        let top = stack.pop();
-        if(top !== map[ch]) return false; 
-    }
+    // else{
+    //     while(stack.length){
+    //         let peakEle = stack[stack.length - 1];
+    //         if(peakEle < arr[i]){
+    //             stack.pop(); 
+    //             outputArray.push(arr[i]);
+    //             continue;
+    //         } 
+    //         break;
+    //     }
+    //     stack.push(arr[i]);
+    // }
+  }
+  while(arr.length !== outputArray.length){
+    outputArray.push(-1);
   }
 
-  return stack.length === 0; 
+  return outputArray;
 }
 
-console.log(isValidParentheses("([{}])"));
-// console.log(isValidParentheses("([)]"));
+let inputArray =  [1, 3, 2, 4];
+console.log(nextGreaterElement(inputArray)); // Output: [3, 4, 4, -1]
+// console.log(nextGreaterElement([3,2,1])); // Output: [-1,-1,-1];
