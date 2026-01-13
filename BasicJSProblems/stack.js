afed8584a070a2915b9d9422b0ada79750635625
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-13 22:29:05 +0500

    more debugging it to find the optimal solution

--- BasicJSProblems/stack.js
+++ BasicJSProblems/stack.js
@@ -77,3 +77,36 @@ function nextGreaterElement(arr) {
 let inputArray =  [1, 3, 2, 4];
 // console.log(nextGreaterElement(inputArray)); // Output: [3, 4, 4, -1]
 console.log(nextGreaterElement([3,2,1])); // Output: [-1,-1,-1];
+
+
+/**************************************** */
+
+function nextGreaterElement(arr) {
+  // your code here
+  let outputArray = [];
+  let stack = [];
+  for(let i=0; i<arr.length; i++){
+    if(stack.length === 0){
+        stack.push(arr[i]);
+    }
+    else{
+        while(stack.length){
+            let peakEle = stack[stack.length - 1];
+            if(peakEle < arr[i]){
+                stack.pop(); 
+                outputArray.push(arr[i]);
+                continue;
+            } 
+            break;
+        }
+        stack.push(arr[i]);
+    }
+  }
+  while(arr.length !== outputArray.length){
+    outputArray.push(-1);
+  }
+
+  return outputArray;
+}
+
+let inputArray =  [1, 3, 2, 4];
