09ed9e4a6cdab95e4a2c75267ae16d49f0ad4fe1
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-14 23:42:22 +0500

    refactor nextGreaterElement function for clarity and efficiency

--- BasicJSProblems/stack.js
+++ BasicJSProblems/stack.js
@@ -48,65 +48,47 @@
 
 function nextGreaterElement(arr) {
   // your code here
+  // create an output array
   let outputArray = [];
+  // create a stack
   let stack = [];
+  // iterate through the input array
   for(let i=0; i<arr.length; i++){
+    // check if the stack is empty
     if(stack.length === 0){
+      // push the current element to the stack
         stack.push(arr[i]);
     }
+    // if the stack is not empty
     else{
+      // check the top element of the stack
         while(stack.length){
+          // compare the top element with the current element
             let peakEle = stack[stack.length - 1];
+            // if the current element is greater than the top element
             if(peakEle < arr[i]){
+              // pop the top element and add the current element to the output array
                 stack.pop(); 
+                // push the current element to the stack
                 outputArray.push(arr[i]);
+                // continue the loop
                 continue;
             } 
+            // if the current element is not greater than the top element
             break;
         }
+        // push the current element to the stack
         stack.push(arr[i]);
     }
   }
+  // for the remaining elements in the stack, add -1 to the output array
   while(arr.length !== outputArray.length){
     outputArray.push(-1);
   }
-
+  // return the output array 
   return outputArray;
 }
 
 let inputArray =  [1, 3, 2, 4];
 // console.log(nextGreaterElement(inputArray)); // Output: [3, 4, 4, -1]
 console.log(nextGreaterElement([3,2,1])); // Output: [-1,-1,-1];
-
-
-/**************************************** */
-
-function nextGreaterElement(arr) {
-  // your code here
-  let outputArray = [];
-  let stack = [];
-  for(let i=0; i<arr.length; i++){
-    if(stack.length === 0){
-        stack.push(arr[i]);
-    }
-    else{
-        while(stack.length){
-            let peakEle = stack[stack.length - 1];
-            if(peakEle < arr[i]){
-                stack.pop(); 
-                outputArray.push(arr[i]);
-                continue;
-            } 
-            break;
-        }
-        stack.push(arr[i]);
-    }
-  }
-  while(arr.length !== outputArray.length){
-    outputArray.push(-1);
-  }
-
-  return outputArray;
-}
-
-let inputArray =  [1, 3, 2, 4];
