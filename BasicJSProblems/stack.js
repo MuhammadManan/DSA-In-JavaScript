6bdfb2cc719c54e6880c60fd54b73449a8665987
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-16 22:43:31 +0500

    feat: implement prevSmaller function for finding nearest smaller elements

--- BasicJSProblems/stack.js
+++ BasicJSProblems/stack.js
@@ -46,26 +46,61 @@
 
 /********************************************************* */
 
-function nextGreaterElement(arr) {
-  // your code here 
-  function nextGreaterElement(arr) {
-  let outputArray = new Array(arr.length).fill(-1);
-  let stack = [];
+// function nextGreaterElement(arr) {
+//   // your code here 
+//   function nextGreaterElement(arr) {
+//   let outputArray = new Array(arr.length).fill(-1);
+//   let stack = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    while (stack.length && arr[stack[stack.length - 1]] < arr[i]) {
-      let index = stack.pop();
-      outputArray[index] = arr[i];
+//   for (let i = 0; i < arr.length; i++) {
+//     while (stack.length && arr[stack[stack.length - 1]] < arr[i]) {
+//       let index = stack.pop();
+//       outputArray[index] = arr[i];
+//     }
+//     stack.push(i);
+//   }
+
+//   return outputArray;
+// }
+
+// }
+
+// console.log(nextGreaterElement([2, 1, 2, 4, 3])); // Output: [4,2,4,-1,-1]
+// let inputArray =  [1, 3, 2, 4];
+// // console.log(nextGreaterElement(inputArray)); // Output: [3, 4, 4, -1]
+// // console.log(nextGreaterElement([3,2,1])); // Output: [-1,-1,-1];
+
+/********************************************************* */
+
+function prevSmaller(arr) {
+    let n = arr.length;
+    let result = new Array(n).fill(-1);
+
+    // stack to keep track of elements
+    let st = [];
+
+    for (let i = 0; i < n; i++) {
+        
+        // pop elements from stack until a smaller 
+        // element is found or stack becomes empty
+        while (st.length > 0 && st[st.length - 1] >= arr[i]) {
+            st.pop();
+        }
+
+        // if stack is not empty, top is nearest smaller
+        if (st.length > 0) {
+            result[i] = st[st.length - 1];
+        }
+
+        // push current element to stack
+        st.push(arr[i]);
     }
-    stack.push(i);
-  }
-
-  return outputArray;
+    return result;
 }
 
-}
+// Driver Code
+let arr = [1, 5, 0, 3, 4, 5];
+let ans = prevSmaller(arr);
+console.log(ans.join(" "));
 
-console.log(nextGreaterElement([2, 1, 2, 4, 3])); // Output: [4,2,4,-1,-1]
-let inputArray =  [1, 3, 2, 4];
-// console.log(nextGreaterElement(inputArray)); // Output: [3, 4, 4, -1]
-// console.log(nextGreaterElement([3,2,1])); // Output: [-1,-1,-1];
+/******************************************* */
