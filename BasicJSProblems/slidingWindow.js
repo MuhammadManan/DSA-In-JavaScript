2acbe29d66fea03367c3fa1b2db22442a47e3fab
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-01-21 17:34:09 +0500

    Recall and add the comments in the max of the sub array sum

--- BasicJSProblems/slidingWindow.js
+++ BasicJSProblems/slidingWindow.js
@@ -1,23 +1,31 @@
-// function maxSubarraySum(arr, k) {
-//   let windowSum = 0;
-//   let maxSum = 0;
+// Sliding Window Pattern - Maximum Subarray Sum of Size K
+function maxSubarraySum(arr, k) {
+  let windowSum = 0;
+  let maxSum = 0;
 
-//   for (let i = 0; i < arr.length; i++) {
-//     windowSum += arr[i];
+  // loop goes through from zero index to the length of the array - 1
+  for (let i = 0; i < arr.length; i++) {
+    // here we are summed up the value in the windowSum variable
+    windowSum += arr[i];
 
-//     if (i >= k - 1) {
-//       maxSum = Math.max(maxSum, windowSum);
-//       windowSum -= arr[i - (k - 1)];
-//     }
-//   }
+    // As we know, we have to check the subArraySum of 3, so whenever
+    // we got the winowSum is summed up by three Elements, 
+    // then the "if" condition is true
+    if (i >= k - 1) {
+      // find the max by apply built-in Math function...
+      maxSum = Math.max(maxSum, windowSum);
+      // remove the value of first element in the windowSum 
+      windowSum -= arr[i - (k - 1)];
+    }
+  }
 
-//   return maxSum;
-// }
+  return maxSum;
+}
 
 
-// let arr = [2, 1, 5, 1, 3, 2], k = 3;
+let arr = [2, 1, 5, 1, 3, 2], k = 3;
 
-// console.log(maxSubarraySum(arr, k));
+console.log(maxSubarraySum(arr, k));
 
 
 /************************************** */
@@ -75,44 +83,44 @@
 
 /**************************************************/
 
- function longestSubstringKDistinct(str, k) {
-  if (k === 0 || str.length === 0) return 0;
+//  function longestSubstringKDistinct(str, k) {
+//   if (k === 0 || str.length === 0) return 0;
 
-  let left = 0;
-  let maxLength = 0;
-  let freq = {};
-  let distinctCount = 0;
+//   let left = 0;
+//   let maxLength = 0;
+//   let freq = {};
+//   let distinctCount = 0;
 
-  for (let right = 0; right < str.length; right++) {
-    // add right character
-    if (freq[str[right]] === undefined) {
-      freq[str[right]] = 1;
-      distinctCount++;
-    } else {
-      freq[str[right]]++;
-    }
+//   for (let right = 0; right < str.length; right++) {
+//     // add right character
+//     if (freq[str[right]] === undefined) {
+//       freq[str[right]] = 1;
+//       distinctCount++;
+//     } else {
+//       freq[str[right]]++;
+//     }
 
-    // shrink window if invalid
-    while (distinctCount > k) {
-      freq[str[left]]--;
-      if (freq[str[left]] === 0) {
-        delete freq[str[left]];
-        distinctCount--;
-      }
-      left++;
-    }
+//     // shrink window if invalid
+//     while (distinctCount > k) {
+//       freq[str[left]]--;
+//       if (freq[str[left]] === 0) {
+//         delete freq[str[left]];
+//         distinctCount--;
+//       }
+//       left++;
+//     }
 
-    // update answer when valid
-    maxLength = Math.max(maxLength, right - left + 1);
-  }
+//     // update answer when valid
+//     maxLength = Math.max(maxLength, right - left + 1);
+//   }
 
-  return maxLength;
-}
+//   return maxLength;
+// }
 
 
-let str = "eceba", k = 2;
-console.log(longestSubstringKDistinct(str,k));
-console.log(longestSubstringKDistinct("eceba", 2)); // 3
-console.log(longestSubstringKDistinct("aa", 1));    // 2
-console.log(longestSubstringKDistinct("abc", 1));   // 1
-console.log(longestSubstringKDistinct("aaaa", 2));  // 4
+// let str = "eceba", k = 2;
+// console.log(longestSubstringKDistinct(str,k));
+// console.log(longestSubstringKDistinct("eceba", 2)); // 3
+// console.log(longestSubstringKDistinct("aa", 1));    // 2
+// console.log(longestSubstringKDistinct("abc", 1));   // 1
+// console.log(longestSubstringKDistinct("aaaa", 2));  // 4
