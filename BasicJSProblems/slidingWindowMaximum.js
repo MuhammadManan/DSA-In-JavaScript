7005647e7cee7d1f56e3c80ccb251b32d0e60fca
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-03 23:52:40 +0500

    Skeleton function to solve the sliding window maximum problem

---
+++ BasicJSProblems/slidingWindowMaximum.js
@@ -0,0 +1,26 @@
+function slidingWindowMax(nums, k) {
+  // result array
+  let result = [];
+
+  // deque to store indices
+  let deque = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    
+    // we will pop ele from the front if it is out of the window
+    
+
+  }
+
+  return result;
+}
+
+let nums = [1,3,-1,-3,5,3,6,7];
+let k = 3;
+
+console.log(slidingWindowMax(nums, k));
+
+// Expected Output:
+// [3,3,5,5,6,7]
+
+
