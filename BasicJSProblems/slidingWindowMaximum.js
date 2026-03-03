95464ac586847b8182209d58d19ca0c241338fec
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-04 00:09:13 +0500

    Solved the Sliding Window Maximum Problem with optimization

--- BasicJSProblems/slidingWindowMaximum.js
+++ BasicJSProblems/slidingWindowMaximum.js
@@ -8,7 +8,22 @@ function slidingWindowMax(nums, k) {
   for (let i = 0; i < nums.length; i++) {
     
     // we will pop ele from the front if it is out of the window
-    
+    if( deque.length !== 0 && deque[0] <= i-k ){
+        deque.shift();
+    }
+
+    // we will pop ele from the back when the new ele come and bigger than them
+    while(deque.length !== 0 && nums[deque[deque.length-1]] <= nums[i]){
+        deque.pop();
+    }
+
+    // otherwise simply add the ele in the deque
+    deque.push(i);
+
+    //here we get the max ele 
+    if(i >= k-1){
+        result.push(nums[deque[0]]);
+    }
 
   }
 
