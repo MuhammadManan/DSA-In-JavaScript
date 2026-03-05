d5550c3f1e884850fc4d34a0b5a808ca97bd2cf1
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-05 17:28:58 +0500

    create linkedlist with previous and next pointers

--- BasicJSProblems/linkedList.js
+++ BasicJSProblems/linkedList.js
@@ -1,47 +1,101 @@
-class node{
+// class node{
+//     constructor(data){
+//         this.value = data;
+//         this.next = null;
+//     }
+// }
+
+// let n1 = new node(10);
+// let n2 = new node(20);
+// let n3 = new node(30);
+// let n4 = new node(40);
+
+// n1.next = n2;
+// n2.next = n3;
+// n3.next = n4;
+
+// //console.log(n1);
+
+// // traverse the LinkedList
+// function traverseLinkedList(head){
+//     let current = head;
+
+//     while(current !== null){
+//         console.log(`${current.value}`);
+//         current = current.next;
+//     }
+// }
+
+// let head = n1;
+// // traverseLinkedList(head);
+
+
+// // Let's Create the LinkedList
+// function createLinkedList(arr) {
+//     let head = new node(arr[0]);
+//     let current = head;
+
+//     for(let i=1; i<arr.length; i++){
+//         current.next = new node(arr[i]);
+//         current = current.next;
+//     }
+
+//     return head;
+// }
+
+// let head1 = createLinkedList([1,2,3,4]);
+// traverseLinkedList(head1);
+
+
+
+//*********************************** */
+
+// Reverse the linkedlist
+
+class Node{
     constructor(data){
+        this.prev = null;
         this.value = data;
         this.next = null;
     }
 }
 
-let n1 = new node(10);
-let n2 = new node(20);
-let n3 = new node(30);
-let n4 = new node(40);
+let n1 = new Node(1);
+let n2 = new Node(2);
+let n3 = new Node(3);
+let n4 = new Node(4);
+// console.log(n1);
 
-n1.next = n2;
-n2.next = n3;
-n3.next = n4;
+// let head = n1;
+// n1.next = n2;
+// n2.prev = n1;
+// n2.next = n3;
+// n3.prev = n2;
+// n3.next = n4;
+// n4.prev = n3;
 
-//console.log(n1);
+// console.log(head);
 
-// traverse the LinkedList
-function traverseLinkedList(head){
-    let current = head;
+// create a dynamic linkedList by passing array
 
-    while(current !== null){
-        console.log(`${current.value}`);
-        current = current.next;
-    }
-}
-
-let head = n1;
-// traverseLinkedList(head);
-
-
-// Let's Create the LinkedList
-function createLinkedList(arr) {
-    let head = new node(arr[0]);
+function createLinkedList(arr){
+    let head = new Node(arr[0]);
     let current = head;
 
     for(let i=1; i<arr.length; i++){
-        current.next = new node(arr[i]);
+        let node = new Node(arr[i]);
+        current.next = node;
+        node.prev = current;
         current = current.next;
     }
 
     return head;
 }
 
-let head1 = createLinkedList([1,2,3,4]);
-traverseLinkedList(head1);
+// write a function to Reverse the linkedList
+function reverseLinkedList(head){
+    
+}
+
+
+let head = createLinkedList([1,2,3,4]);
