03f99dfaa8dcc7dd8f23256cd004c544106ecba2
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-05 22:27:48 +0500

    All code are related to the doubly linkedlist and still reverse problem are need solve again using single linkedlist

--- BasicJSProblems/linkedList.js
+++ BasicJSProblems/linkedList.js
@@ -52,66 +52,78 @@
 
 // Reverse the linkedlist
 
-class Node{
-    constructor(data){
-        this.prev = null;
-        this.value = data;
-        this.next = null;
-    }
-}
+// class Node{
+//     constructor(data){
+//         this.prev = null;
+//         this.value = data;
+//         this.next = null;
+//     }
+// }
 
-let n1 = new Node(1);
-let n2 = new Node(2);
-let n3 = new Node(3);
-let n4 = new Node(4);
-// console.log(n1);
+// let n1 = new Node(1);
+// let n2 = new Node(2);
+// let n3 = new Node(3);
+// let n4 = new Node(4);
+// // console.log(n1);
 
-// let head = n1;
-// n1.next = n2;
-// n2.prev = n1;
-// n2.next = n3;
-// n3.prev = n2;
-// n3.next = n4;
-// n4.prev = n3;
+// // let head = n1;
+// // n1.next = n2;
+// // n2.prev = n1;
+// // n2.next = n3;
+// // n3.prev = n2;
+// // n3.next = n4;
+// // n4.prev = n3;
 
-// console.log(head);
+// // console.log(head);
 
-// create a dynamic linkedList by passing array
+// // create a dynamic linkedList by passing array
 
-function createLinkedList(arr){
-    if(arr.length === 0 ) return "Array is Empty";
+// function createLinkedList(arr){
+//     if(arr.length === 0 ) return "Array is Empty";
 
-    let head = new Node(arr[0]);
-    let current = head;
+//     let head = new Node(arr[0]);
+//     let current = head;
 
-    for(let i=1; i<arr.length; i++){
-        let node = new Node(arr[i]);
-        current.next = node;
-        node.prev = current;
-        current = current.next;
-    }
+//     for(let i=1; i<arr.length; i++){
+//         let node = new Node(arr[i]);
+//         current.next = node;
+//         node.prev = current;
+//         current = current.next;
+//     }
 
-    return head;
-}
+//     return head;
+// }
 
-// write a function to Reverse the linkedList
-function reverseLinkedList(head){
-    // console.log("head: ", head);
-    let current = head;
-    // console.log("current", current);
+// // write a function to Reverse the linkedList
+// function reverseLinkedList(head){
+//     // console.log("head: ", head);
+//     let current = head;
+//     // console.log("current", current);
 
-    while(true){
-        if(current.next === null){
-            head = current;
-            break;
-        }
-        current = current.next;
-    }
-    // console.log("current", current);
-    return head;
-}
+//     while(true){
+//         if(current.next === null){
+//             head = current;
+//             break;
+//         }
+//         current = current.next;
+//     }
+//     // console.log("current", current);
+//     return head;
+// }
+
+// // write a function to traverse the LinkedList
+// function traverseLinkedList(head){
+//     let current = head;
+
+//     while(current !== null){
+//         console.log(current.value);
+//         current = current.prev;
+//     }
+
+// }
 
 
-let head = createLinkedList([1,2,3,4]);
-let revHead = reverseLinkedList(head);
-// console.log("revHead: ", revHead);
+// let head = createLinkedList([1,2,3,4]);
+// let revHead = reverseLinkedList(head);
+// // console.log("revHead: ", revHead);
+// traverseLinkedList(revHead);
