6e197316e32daad215bc7a84ccf1aa1dae27e4db
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-22 23:16:32 +0500

    Add sumOfDigits function to calculate the sum of digits of a number

--- BasicJSProblems/math.js
+++ BasicJSProblems/math.js
@@ -53,3 +53,12 @@ function fibonacci(n){
 }   
 
 console.log(fibonacci(10));
+
+// function to find the sum of digits of a number
+
+function sumOfDigits(num){
+    if(num === 0) return 0;
+    return (num % 10) + sumOfDigits(Math.floor(num / 10));
+}
+
+console.log(sumOfDigits(1234));
