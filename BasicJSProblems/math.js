0605fc9982e18161d2475e6556f4854f71653203
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-23 23:19:58 +0500

    Add countDigits function to calculate the number of digits in a number

--- BasicJSProblems/math.js
+++ BasicJSProblems/math.js
@@ -90,3 +90,11 @@ function sumOfDigits(num){
 }
 
 console.log(sumOfDigits(1234));
+
+// function to find the number of digits in a number
+function countDigits(num){
+    if(num === 0) return 0;
+    return 1 + countDigits(Math.floor(num / 10));
+}
+
+console.log(countDigits(1234));
