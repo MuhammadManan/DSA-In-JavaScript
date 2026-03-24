c1a16bf117a2629a517c50d3b12808e996f17387
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-24 23:40:23 +0500

    Add factorial function implementation and ensure proper newline at end of file

--- BasicJSProblems/math.js
+++ BasicJSProblems/math.js
@@ -121,3 +121,11 @@ function power(base, exponent){
 }
 
 console.log(power(2, 3));
+
+// function to find the factorial of a number
+function factorial(n){
+    if(n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
