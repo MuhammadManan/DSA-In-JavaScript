09996f9fc06cdb9ac49fca7bdca797bf4c2c7f7b
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-03-27 21:57:02 +0500

    Add extractDigit function to print each digit of a number and ensure proper newline at end of file

--- BasicJSProblems/math.js
+++ BasicJSProblems/math.js
@@ -129,3 +129,16 @@ function factorial(n){
 }
 
 console.log(factorial(5));
+
+// function to extract the input digit
+function extractDigit(num){
+    let N = num;
+    let lastDigit;
+    while(N > 0){
+        lastDigit = N % 10;
+        console.log(lastDigit);
+        N = Math.floor(N / 10);
+    }
+}
+
+extractDigit(1234);
