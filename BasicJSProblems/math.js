ef617c48bbb4d97bff666d7801e2775e44025bbb
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2026-04-03 22:52:57 +0500

    Add islandPerimeter function to calculate the perimeter of an island in a grid

--- BasicJSProblems/math.js
+++ BasicJSProblems/math.js
@@ -296,3 +296,27 @@ function height(node, diameter){
 
     return 1 + Math.max(l,r);
 }
+
+
+// find the island perimeter
+function islandPerimeter(grid){
+    let perimeter = 0;
+    for(let i=0; i<grid.length; i++){
+        for(let j=0; j<grid[0].length; j++){
+            if(grid[i][j] === 1) perimeter += 4 - countNeighbors(grid, i, j);
+        }
+    }
+    return perimeter;
+}
+
+function countNeighbors(grid, i, j){
+    let count = 0;
+    if(i > 0 && grid[i-1][j] === 1) count++;
+    if(i < grid.length - 1 && grid[i+1][j] === 1) count++;
+    if(j > 0 && grid[i][j-1] === 1) count++;
+    if(j < grid[0].length - 1 && grid[i][j+1] === 1) count++;
+    return count;
+}
+
+
+console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]));
